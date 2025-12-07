18a54df6c85bbb528878227bb50a14e199c4884c
WesleyyDev <wesleyqueirozdeveloper@gmail.com>
2025-12-07 19:29:52 -0300

    fix: update bot presence activity name to 'Engenheiro de Software'

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ async def on_ready():
 
     activity_watching = discord.Activity(
         type=discord.ActivityType.watching,
-        name='/help'
+        name='Engenheiro de Software'
     )
 
     await bot.change_presence(activity=activity_watching, status=discord.Status.online)
